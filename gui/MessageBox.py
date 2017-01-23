091467e37c672b9a880ab3494b8c74f110d590bb
Sam <samuelgoodin@sbcglobal.net>
2017-01-23 09:07:14 -0500

    MessageBox done. Toon: stickerbook Anims Estate: houses

--- gui/MessageBox.py
+++ gui/MessageBox.py
@@ -1,7 +1,6 @@
 from direct.gui.DirectGui import *
 from pandac.PandaModules import *
 import Globals
-from direct.showbase.Transitions import Transitions
 
 
 class MessageBox(DirectFrame):
@@ -14,9 +13,15 @@ class MessageBox(DirectFrame):
                              geom_pos=Point3(0, 0, 0), geom_scale=gscale, pos=(0, 0, 0))
         self.initialiseoptions(MessageBox)
         self.setScale(.8)
-        string =
-        self.label = DirectLabel(parent=self, relief=None, pos=(0, 0, .2), text=string, text_scale=0.1)
-        self.label['text'].setWordwrap(10)
+        self.message = TextNode('Message')
+        self.message.setText(string)
+        self.message.setWordwrap(1)
+        self.message.setTextScale(0.15)
+        self.message.setTextColor(0, 0, 0, 1)
+        self.message.setAlign(TextNode.ACenter)
+        self.message.setFont(loader.loadFont('phase_3/models/fonts/ImpressBT.ttf'))
+        self.nodePath = self.attachNewNode(self.message)
+        self.nodePath.setPos(0, 0, .2)
         self._battleGui = loader.loadModel('phase_3/models/gui/dialog_box_buttons_gui')
         self.okayButton = DirectButton(parent=self, relief=None,
                                        image=(self._battleGui.find('**/ChtBx_OKBtn_UP'),
@@ -24,8 +29,8 @@ class MessageBox(DirectFrame):
                                               self._battleGui.find('**/ChtBx_OKBtn_Rllvr')),
                                        pos=(0.0, 0, -0.19), scale=(0.39, 1.0, 0.39), image_scale=(4, 4, 4),
                                        text=('', 'Okay', 'Okay', ''),
-                                       text_scale=Globals.RPskipScale, text_fg=Vec4(1, 1, 1, 1),
-                                       text_shadow=Vec4(0, 0, 0, 1), text_pos=Globals.RPskipPos, textMayChange=0,
+                                       text_scale=Globals.RPskipScale, text_fg=Vec4(0, 0, 0, 1),
+                                       text_shadow=None, text_pos=Globals.RPskipPos, textMayChange=0,
                                        command=self.destroy)
         self.setScale(.513544, .513544, .513544)
         loader.unloadModel(geom)
@@ -37,7 +42,7 @@ class MessageBox(DirectFrame):
         if base.marginManager.getIsPositionAvailable():
             self.pos = base.marginManager.getRandomOpenPos()
             self.setPos(self.pos)
-            taskMgr.doMethodLater(2, self.delete, 'Popup')
+            taskMgr.doMethodLater(10, self.delete, 'Popup')
         else:
             self.pos = None
             taskMgr.add(self.delete, 'Popup')
@@ -47,7 +52,8 @@ class MessageBox(DirectFrame):
         return task.done
 
     def destroy(self):
-        self.label.destroy()
+        self.message = None
+        self.nodePath = None
         self.okayButton.destroy()
         DirectFrame.destroy(self)
         if self.pos:
